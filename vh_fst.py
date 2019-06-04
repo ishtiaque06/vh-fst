bf95aa00d7a87cf0ef1a1696e6212ed7fc268e89
Ahmed Ishtiaque <ahmedishti27@gmail.com>
2019-06-03 23:01:49 -0400

    Added dummy init method

--- vh_fst.py
+++ vh_fst.py
@@ -2,7 +2,7 @@
     FST class to map words in a set of languages to their expected vowel
     pronunciations
 --------------------------------------------------------------------------AI'''
-
-
 class FST:
-    pass
+
+    def __init__(self, language):
+        pass
