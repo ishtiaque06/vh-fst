d36d249720da7efe36e7139f2706e47dc0c27d40
therringsh <51343881+therringsh@users.noreply.github.com>
2019-06-06 17:10:37 -0400

    Update vh_patterns_dataset.py

--- vh_patterns_dataset.py
+++ vh_patterns_dataset.py
@@ -10,7 +10,7 @@ list_desc = ['VH pattern', 'State set','Alphabet of relevant symbols',
 
 vh_dataset={1:
                 {
-                    'name': 'Kisa applicative suffix',
+                    'name': 'Kisa applicative suffix Vl'+B_L_U_NT,
                     'states': {0:'il'+B_L_U_NT, 1:'il'+B_L_U_NT, 2:'el'+B_L_U_NT},
                     'alphabet': ['i','e','u','o'],
                     'transitions': {(0, '?'): ('?', 0), (0, 'i'): ('i', 1),
@@ -29,7 +29,7 @@ vh_dataset={1:
                 },
             2:
                 {
-                    'name': 'Kisa reversative suffix',
+                    'name': 'Kisa reversative suffix -Vl'+B_L_U_NT,
                     'states': {0:'ul'+B_L_U_NT, 1:'ul'+B_L_U_NT, 2:'ol'+B_L_U_NT},
                     'alphabet': ['u','o'],
                     'transitions': {(0, '?'): ('?', 0), (0, 'u'): ('u', 1),
@@ -83,5 +83,75 @@ vh_dataset={1:
                     'left_subseq': True,
                     'notes': ['Backness harmonizes', 'pairs are of the same height',
                         'No neutral vowels']
+                },
+            #5P is the attributes for which the FST must be run for selections 5,6, and 7 prior to running their respective attributes
+            #I.e., the order is User input=> language-relevant preprocessing=> input into 5P=>output is the input for 5, 6, 7
+            5P:
+                 {
+                    'name': 'Uyghur Preliminary rounding harmony',
+                    'states': {0:'',1:'',2:''},
+                    'alphabet': ['i','e','y',F_M_R_T],
+                    'transitions': {(0,'?'):('?',0),(1,'?'):('?',1),(2,'?'):('?',2),(0,'i'):('i',1),(0,'e'):('e',1),(0,'y'):('y',2), (0,F_M_R_T):(F_M_R_T,2),(1,'y'):('i',1),(1,F_M_R_T):('e',1),(2,'i'):('y',2),(2,'e'):(F_M_R_T,2)}
+                 },
+            5: #needs preprocessing with 5P
+                {
+                    'name': 'Uyghur backness harmony',
+                    'states': {0:'',1:'',2:''},
+                    'alphabet': ['i','y','u','e',F_M_R_T,'a','o',B_L_U_NT],
+                    'transitions': {(0,'?'):('?',0), (1,'?'):('?',1),(2,'?'):('?',2),
+                        (0,'i'):('i',0), (1,'i'):('i',1),(2,'i'):('i',2),
+                        (0,'e'):('e',0), (1,'e'):('e',1),(2,'e'):('e',2),
+                        (0,'a'):('a',1), (0,'y'):('y' ,1), (0,F_M_R_T):(F_M_R_T,1),
+                        (0,'u'):('u',2), (0,B_L_U_NT):(B_L_U_NT,2), (0,'o'):('o',2),
+                        (1,F_M_R_T):(F_M_R_T,1), (1,'y'):('y' ,1),(1,'a'):('a' ,1),
+                        (2,'u'):('u',2), (2,B_L_U_NT):(B_L_U_NT ,2), (2,'o'):('o',2),
+                        (1,'u'):('y' ,1),(1,B_L_U_NT):('a' ,1),(1,'o'):(F_M_R_T,1), 
+                        (2,'y'):('u',2), (2,'a'):(B_L_U_NT,2), (2,F_M_R_T):('o',2)},
+                    'preprocess_req': False,
+                    'postprocess_req': False,
+                    'left_subseq': True,
+                    'preprocess_dets': 'Preprocess by running initial input through FST with 5P as parameters
+                    'notes': ['Backness harmonizes', 'pairs are of the same height',
+                        'i and e are transparent neutral vowels','there is rounding harmony i:y,e:FMRT']
+                },
+             6: #needs preprocessing with 5P
+                {
+                    'name': 'Uyghur plural suffix -lVr',
+                    'states': {0:'l'+B_L_U_NT+'r',1:'lar',2:'l'+B_L_U_NT+'r'},
+                    'alphabet': ['i','y','u','e',F_M_R_T,'a','o',B_L_U_NT],
+                    'transitions': {(0,'?'):('?',0), (1,'?'):('?',1),(2,'?'):('?',2),
+                        (0,'i'):('i',0), (1,'i'):('i',1),(2,'i'):('i',2),
+                        (0,'e'):('e',0), (1,'e'):('e',1),(2,'e'):('e',2),
+                        (0,'a'):('a',1), (0,'y'):('y' ,1), (0,F_M_R_T):(F_M_R_T,1),
+                        (0,'u'):('u',2), (0,B_L_U_NT):(B_L_U_NT,2), (0,'o'):('o',2),
+                        (1,F_M_R_T):(F_M_R_T,1), (1,'y'):('y' ,1),(1,'a'):('a' ,1),
+                        (2,'u'):('u',2), (2,B_L_U_NT):(B_L_U_NT ,2), (2,'o'):('o',2),
+                        (1,'u'):('y' ,1),(1,B_L_U_NT):('a' ,1),(1,'o'):(F_M_R_T,1), 
+                        (2,'y'):('u',2), (2,'a'):(B_L_U_NT,2), (2,F_M_R_T):('o',2)},
+                    'preprocess_req': True,
+                    'postprocess_req': False,
+                    'left_subseq': True,
+                    'preprocess_dets': 'Preprocess by removing last three characters and then running this shortened input through FST with 5P as parameters'
+                    'notes': ['DOES NOT WORK FOR DISHARMONIC LOAN WORDS','Backness harmonizes','i and e are transparent neutral vowels','[+back] default for the suffix','there is rounding harmony i:y,e:FMRT']
+                },
+            7: #needs preprocessing with 5P
+                {
+                    'name': 'Uyghur dative suffix'+U_F_V+'V',
+                    'states': {0: U_F_V+B_L_U_N_T,1:'ga',2:U_F_V+B_L_U_NT},
+                    'alphabet': ['i','y','u','e',F_M_R_T,'a','o',B_L_U_NT],
+                    'transitions': {(0,'?'):('?',0), (1,'?'):('?',1),(2,'?'):('?',2),
+                        (0,'i'):('i',0), (1,'i'):('i',1),(2,'i'):('i',2),
+                        (0,'e'):('e',0), (1,'e'):('e',1),(2,'e'):('e',2),
+                        (0,'a'):('a',1), (0,'y'):('y' ,1), (0,F_M_R_T):(F_M_R_T,1),
+                        (0,'u'):('u',2), (0,B_L_U_NT):(B_L_U_NT,2), (0,'o'):('o',2),
+                        (1,F_M_R_T):(F_M_R_T,1), (1,'y'):('y' ,1),(1,'a'):('a' ,1),
+                        (2,'u'):('u',2), (2,B_L_U_NT):(B_L_U_NT ,2), (2,'o'):('o',2),
+                        (1,'u'):('y' ,1),(1,B_L_U_NT):('a' ,1),(1,'o'):(F_M_R_T,1), 
+                        (2,'y'):('u',2), (2,'a'):(B_L_U_NT,2), (2,F_M_R_T):('o',2)},
+                    'preprocess_req': True,
+                    'postprocess_req': False,
+                    'left_subseq': True,
+                    'preprocess_dets': 'Preprocess by removing last two characters and then running this shortened input through FST with 5P as parameters'
+                    'notes': ['DOES NOT WORK FOR DISHARMONIC LOAN WORDS','Backness harmonizes','i and e are transparent neutral vowels','[+back] default for the suffix','there is rounding harmony i:y,e:FMRT']
                 }
-            }
+             }
